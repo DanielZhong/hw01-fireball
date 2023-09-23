2f1f8a258f0d43bb7fd42051d02d9823a42515b1
Daniel <danielzhong2000@gmail.com>
2023-09-23 15:34:27 -0400

    update

--- src/rendering/gl/OpenGLRenderer.ts
+++ src/rendering/gl/OpenGLRenderer.ts
@@ -22,9 +22,18 @@ class OpenGLRenderer {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   }
 
-  render(camera: Camera, prog: ShaderProgram, drawables: Array<Drawable>, time: number) {
-    prog.setEyeRefUp(camera.controls.eye, camera.controls.center, camera.controls.up);
-    prog.setTime(time);
+  render(camera: Camera, prog: ShaderProgram, drawables: Array<Drawable>,  MyColor: vec4, MyScale: GLfloat, Persistency: GLfloat, Transparency: GLfloat) {
+    let model = mat4.create();
+    let viewProj = mat4.create();
+
+    mat4.identity(model);
+    mat4.multiply(viewProj, camera.projectionMatrix, camera.viewMatrix);
+    prog.setModelMatrix(model);
+    prog.setViewProjMatrix(viewProj);
+    prog.setGeometryColor(MyColor);
+    prog.setScale(MyScale);
+    prog.setPersistency(Persistency);
+    prog.setTransparency(Transparency);
 
     for (let drawable of drawables) {
       prog.draw(drawable);
